d79d426b300e9bcf373a38944daf8a60d31e2201
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-01 15:07:03 +0530

    Added: the functionality to add values in linked list

--- src/com/bridgelabz/linkedList/LinkedList.java
+++ src/com/bridgelabz/linkedList/LinkedList.java
@@ -1,9 +1,35 @@
 package com.bridgelabz.linkedList;
 
 public class LinkedList {
+	MyNode head;
+	
+
+	public void add(int data)
+	{
+		MyNode newNode = new MyNode();
+		newNode.data = data;
+		newNode.next = head;
+		head = newNode;
+		
+	}
+	public void print()
+	{
+		MyNode tempNode = head;
+		while(tempNode.next != null)
+		{
+			System.out.println(tempNode.data);
+			tempNode=tempNode.next;
+		}
+		System.out.println(tempNode.data);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to linked list");
+		LinkedList linkedList = new LinkedList();
+		System.out.println("By adding the values in the list ");
+		linkedList.add(70);
+		linkedList.add(30);
+		linkedList.add(56);
+		linkedList.print();
 	}
 
 }
---
+++ src/com/bridgelabz/linkedList/MyNode.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.linkedList;
+
+public class MyNode {
+	
+	int data =0;
+	MyNode next;
+	
+	public MyNode(int data, MyNode next) {
+		super();
+		this.data = data;
+		this.next = null;
+	}
+	public MyNode() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public MyNode getNext() {
+		return next;
+	}
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+
+}
