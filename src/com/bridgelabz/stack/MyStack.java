a78a10536bf043b843df87a112171cb8bb335510
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-02 21:37:01 +0530

    Added: functionality pop first value till stack is empty

--- src/com/bridgelabz/stack/MyStack.java
+++ src/com/bridgelabz/stack/MyStack.java
@@ -3,12 +3,11 @@ package com.bridgelabz.stack;
 import com.bridgelabz.linkedList.LinkedListExample;
 import com.bridgelabz.linkedList.MyNode;
 
-public class MyStack 
-{
+public class MyStack {
+
 	public final LinkedListExample mylinkedList;
 	
-	public MyStack()
-	{
+	public MyStack() {
 		super();
 		this.mylinkedList = new LinkedListExample();
 	}
@@ -21,19 +20,32 @@ public class MyStack
 	{
 		mylinkedList.print();
 	}
-	public void pop() 
-	{
+	public void pop() {
 		mylinkedList.pop();
 	}
-
-	public static void main(String[] args) 
+	
+	public void peak() 
 	{
+		MyNode value = mylinkedList.head;
+		while(value !=null)
+		{
+		System.out.println("Peak vaalue is :" +value.getData());
+		pop();
+		System.out.println(value);
+		value = value.getNext();
+		}
+		System.out.println("Stack is Empty now");
+	}
+
+	public static void main(String[] args) {
 		MyStack myStack = new MyStack();
 		myStack.push(70);
 		myStack.push(30);
 		myStack.push(56);
 		System.out.println("The Stack is:");
 		myStack.print();
+		System.out.println();
+		myStack.peak();
 	}
 
 }
